81d8e79f14a88ee46ddc82aec97912bed4c38c1e
jovishacurlie <jovishacurlie@gmail.com>
2025-05-23 23:32:26 +0530

    changed req.txt

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-for course in courses:
-    #     course.lessons = Lesson.query.filter_by(course_id=course.course_id).all()
-    #     course.quizzes = Quiz.query.filter_by(course_id=course.course_id).all()
